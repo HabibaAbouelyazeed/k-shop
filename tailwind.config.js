2d1022a21c6b20c01cf9f5b502af51694223d694
Habiba Mahmoud <habibaabouelyazeed@gmail.com>
2023-11-14 23:36:58 +0200

    header

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'primary': '#302CA2',
+      'primary-100': 'rgba(48, 44, 162, 0.08)',
+      'primary-accent': '#23104E',
+      'secondary': '#26A37C',
+      'txt-black': '#032326',
+      'divider-gray': '#B7AECA',
+      'foreGround': '#FFFFFF',
+      'starOn': '#FFE621',
+      'starOff': '#EAEAEA',
+      'background': '#F6F6F6' 
+    },
+    extend: {
+      fontFamily:{
+        'cairo': ['Cairo', ...defaultTheme.fontFamily.sans]
+      }
+    },
+  },
+  plugins: [],
+}
+
